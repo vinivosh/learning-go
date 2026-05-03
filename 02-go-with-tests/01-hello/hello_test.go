e6834d1b5da3ff6afb054103c9ee045b49945a32
vinivosh <36175157+vinivosh@users.noreply.github.com>
2026-05-03 15:40:10 -0300

    refactor(go-with-tests): ✅ Improve tests by adding a helper function

--- 02-go-with-tests/01-hello/hello_test.go
+++ 02-go-with-tests/01-hello/hello_test.go
@@ -2,21 +2,24 @@ package main
 
 import "testing"
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
 func TestHello(t *testing.T) {
 	t.Run("say hello to someone specific", func(t *testing.T) {
 		got := Hello("Vini")
 		want := "Hello, Vini"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
